07e145ba4156f6d3b026e912c062269b79da8da5
zingotaku <zingvirus@gmail.com>
2021-12-07 17:57:43 -0300

    Atualizacao

---
+++ cpfiles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp $HOME/.config/i3/config ./i3gaps.config
+cp $HOME/.config/polybar/config ./polybar.config
+cp -r $HOME/.config/polybar/scripts ./polybar.scripts
+cp $HOME/.config/picom/picom.conf .
+cp $HOME/.config/alacritty/alacritty.yml .
